e6a1a4a91b8e56b8cf9af04682453361d18ea866
Geagcec <1216060397@qq.com>
2026-02-20 18:45:13 +0800

    Add files via upload

---
+++ prodducts.js
@@ -0,0 +1,54 @@
+// products.js - 商品数据文件
+// 格式：每个商品是一个对象，包含以下字段
+const products = [
+    {
+        id: 1,
+        name: "环保帽",
+        price: "2 Gea",
+        stock: 10,
+        image: "product1.png",
+        category: "环保帽",
+        description: "来图定制环保帽",
+        sales: "" // 销量（可选）
+    },
+    {
+        id: 2,
+        name: "无纺布袋",
+        price: "2 Gea",
+        stock: 15,
+        image: "product2.png",
+        category: "环保袋",
+        description: "无纺布袋定做 logo可印字 包邮",
+        sales: ""
+    },
+    {
+        id: 3,
+        name: "环保夹",
+        price: "1.3 Gea",
+        stock: 8,
+        image: "product3.png",
+        category: "环保工具",
+        description: "加长铝合金垃圾夹子",
+        sales: ""
+    },
+    {
+        id: 4,
+        name: "环保海报",
+        price: "1 Gea",
+        stock: 20,
+        image: "product4.png",
+        category: "环保宣传海报",
+        description: "环保宣传海报PSD",
+        sales: ""
+    },
+    {
+        id: 5,
+        name: "环保马甲",
+        price: "2 Gea",
+        stock: 12,
+        image: "product5.png",
+        category: "环保马甲",
+        description: "环保马甲 宽松版",
+        sales: ""
+    }
+];
